81af55cabc3691b32f1292c06447210f1db09d0d
Ariel Schwartz <schwartz254@gmail.com>
2022-12-14 20:18:10 +0200

    Adding solution to question 2

--- Solutions/Ariel_Schwartz_solutions.js
+++ Solutions/Ariel_Schwartz_solutions.js
@@ -1,2 +1,5 @@
 // 1) Write a function identity that takes an argument and returns that argument
 const identity = (argument) => argument;
+
+// 2) Write a binary function addb that takes two numbers and returns their sum
+const addb = (a, b) => a + b;
