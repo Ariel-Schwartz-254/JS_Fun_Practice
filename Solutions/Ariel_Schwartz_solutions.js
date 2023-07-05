c0692676b1113cbc8576dd088bbb1cb8fea62140
Ariel Schwartz <schwartz254@gmail.com>
2023-07-05 11:00:45 +0300

    Adding solutions to questions 7-10

--- Solutions/Ariel_Schwartz_solutions.js
+++ Solutions/Ariel_Schwartz_solutions.js
@@ -15,3 +15,118 @@ const minb = (a, b) => Math.min(a, b);
 
 // 6) Write a binary function maxb that takes two numbers and returns the larger one
 const maxb = (a, b) => Math.max(a, b);
+
+// 7) Write a function add that is generalized for any amount of arguments
+const add = (...nums) => {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    return sum;
+}
+
+// 8) Write a function sub that is generalized for any amount of arguments
+const sub = (...nums) => {
+    let sum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        sum -= nums[i];
+    }
+    return sum;
+}
+
+// 9) Write a function mul that is generalized for any amount of arguments
+const mul = (...nums) => {
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        product *= nums[i];
+    }
+    return product;
+}
+
+// 9) Write a function min that is generalized for any amount of arguments
+const min = (...nums) => Math.min(...nums);
+
+// 10) Write a function max that is generalized for any amount of arguments
+const max = (...nums) => Math.max(...nums);
+
+module.exports = {
+    identity,
+    addb,
+    subb,
+    mulb,
+    minb,
+    maxb,
+    add,
+    sub,
+    mul,
+    min,
+    max,
+    // addRecurse,
+    // mulRecurse,
+    // minRecurse,
+    // maxRecurse,
+    // not,
+    // acc,
+    // accPartial,
+    // accRecurse,
+    // fill,
+    // fillRecurse,
+    // set,
+    // identityf,
+    // addf,
+    // liftf,
+    // pure,
+    // curryb,
+    // curry,
+    // inc,
+    // twiceUnary,
+    // doubl,
+    // square,
+    // twice,
+    // reverseb,
+    // reverse,
+    // composeuTwo,
+    // composeu,
+    // composeb,
+    // composeTwo,
+    // compose,
+    // limitb,
+    // limit,
+    // genFrom,
+    // genTo,
+    // genFromTo,
+    // elementGen,
+    // element,
+    // collect,
+    // filter,
+    // filterTail,
+    // concatTwo,
+    // concat,
+    // concatTail,
+    // gensymf,
+    // gensymff,
+    // fibonaccif,
+    // counter,
+    // revocableb,
+    // revocable,
+    // extract,
+    // m,
+    // addmTwo,
+    // addm,
+    // liftmbM,
+    // liftmb,
+    // liftm,
+    // exp,
+    // expn,
+    // addg,
+    // liftg,
+    // arrayg,
+    // continuizeu,
+    // continuize,
+    // vector,
+    // exploitVector,
+    // vectorSafe,
+    // pubsub,
+    // mapRecurse,
+    // filterRecurse,
+};
